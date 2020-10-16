6836004e3911516c787e46a1097ccb2db6578432
kstdbrv <kstdbrv1@gmail.com>
2020-10-16 16:01:29 +0300

    add__redux

--- MasterHands/frontend/src/App.js
+++ MasterHands/frontend/src/App.js
@@ -3,9 +3,10 @@ import Layout from './hoc/Layout/Layout'
 import { Route, Switch } from 'react-router-dom'
 import Services from './containers/services/Services'
 import './App.scss'
-import Categories from './containers/categories/Categories';
-import store from './store/store';
-import { Provider } from 'react-redux';
+import Categories from './containers/categories/Categories'
+import store from './store/store'
+import { Provider } from 'react-redux'
+
 
 
 
