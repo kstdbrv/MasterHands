dd78aba6141b403eb0928b8ded8d5b275bb6b2de
Serega <samkreepertrololo@mail.ru>
2020-10-22 09:20:53 +0300

    dwad

--- MasterHands/frontend/src/components/header/Header.js
+++ MasterHands/frontend/src/components/header/Header.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './header.scss'
+import { useEffect } from 'react' 
 
-export const Header = () => (
+export const Header = () => {
 
-  <header className="header">
+ 
+  useEffect(() => {
+    var body  = document.querySelector("body");
+    var menu  = document.getElementById("menu__toggle");
+    var burg  = document.querySelector(".menu__btn span");
+    var show  = document.getElementById("show-more");
+    var arrow = document.getElementById("show-more");
+    
+    menu.addEventListener('change', () => {
+        if(menu.checked){
+            body.style.overflow = "hidden";
+        } else {
+            body.style.overflow = "";
+        }
+    })
+
+    document.addEventListener('scroll', () =>{
+        if ( window.scrollY > 60){
+                 burg.style.background = "#0F0F0F";
+             } else {
+                burg.style.background = "white";
+             }
+    })
+
+    show.addEventListener('click', function(e){
+        var visible = document.querySelectorAll(".speciality__works .visible");
+        arrow.classList.toggle('show-more__active');
+        visible.forEach(function(v) {
+            v.style.display = v.style.display === 'block' ? 'none' : 'block';
+        });
+       
+    })
+}, [])
+
+  return(
+    <header className="header">
     <div className="container">
     <div className="header__inner">
     <div className="header__logo">
@@ -70,5 +106,6 @@ export const Header = () => (
       </div>
       </div>
     </header>
-)
+  )
+}
 
