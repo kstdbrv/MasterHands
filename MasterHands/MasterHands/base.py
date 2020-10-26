64e13ae326d27bff7614787f3414042f1eecdc33
Stefan Vasilenko <vasilencko.stef2013@yandex.ru>
2020-10-26 18:14:12 +0300

    create custom base

--- MasterHands/MasterHands/base.py
+++ MasterHands/MasterHands/base.py
@@ -143,14 +143,11 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:8000',
     'http://localhost:8080',
+    'http://77.222.63.249',
 )
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
---
+++ MasterHands/MasterHands/production.py
@@ -0,0 +1,4 @@
+from .base import *
+
+DEBUG = False
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
