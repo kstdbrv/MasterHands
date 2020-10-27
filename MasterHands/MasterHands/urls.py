9bda501c6bae37f0d23df2a49771a8d221bbff5d
Stefan Vasilenko <vasilencko.stef2013@yandex.ru>
2020-10-27 10:02:21 +0300

    Fix template name

--- MasterHands/MasterHands/urls.py
+++ MasterHands/MasterHands/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
-    path('', include('main.urls')),
+    path('api/', include('api.urls')),
     path('', include('smart_selects.urls')),
+    path('', include('main.urls')),
 ]
