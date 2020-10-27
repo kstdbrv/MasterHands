6b2585ace03488f8ec4427eee80bf4acae2558d6
Stefan Vasilenko <vasilencko.stef2013@yandex.ru>
2020-10-27 10:03:23 +0300

    Fix url routes

--- MasterHands/api/urls.py
+++ MasterHands/api/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/supercategories/', views.SupercategoryListCreate.as_view()),
-    path('api/supercategories/<int:pk>', views.SupercategoryRetrieveUpdateDestroy.as_view()),
-    path('api/categories/', views.CategoryListCreate.as_view()),
-    path('api/categories/<int:pk>', views.CategoryRetrieveUpdateDestroy.as_view()),
-    path('api/services/', views.ServiceListCreate.as_view()),
-    path('api/services/<int:pk>', views.SeviceRetrieveUpdateDestroy.as_view()),
-    path('api/subcategories/', views.SubcategoryListCreate.as_view()),
-    path('api/subcategories/<int:pk>', views.SubcategoryRetrieveUpdateDestroy.as_view()),
+    path('supercategories/', views.SupercategoryListCreate.as_view()),
+    path('supercategories/<int:pk>', views.SupercategoryRetrieveUpdateDestroy.as_view()),
+    path('categories/', views.CategoryListCreate.as_view()),
+    path('categories/<int:pk>', views.CategoryRetrieveUpdateDestroy.as_view()),
+    path('services/', views.ServiceListCreate.as_view()),
+    path('services/<int:pk>', views.SeviceRetrieveUpdateDestroy.as_view()),
+    path('subcategories/', views.SubcategoryListCreate.as_view()),
+    path('subcategories/<int:pk>', views.SubcategoryRetrieveUpdateDestroy.as_view()),
 ]
