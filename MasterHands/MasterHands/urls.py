2b0ff6c406269d5517220d16c67e81698700895b
Stefan Vasilenko <vasilencko.stef2013@yandex.ru>
2020-10-28 22:50:35 +0300

    re merge commit

--- MasterHands/MasterHands/urls.py
+++ MasterHands/MasterHands/urls.py
@@ -23,6 +23,5 @@ urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('api/', include('api.urls')),
     path('', include('smart_selects.urls')),
     path('', include('main.urls')),
-
 ]
 
