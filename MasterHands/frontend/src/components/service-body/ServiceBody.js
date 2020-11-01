2c86b94db3200be5fa26ac17e01192fc8223a107
kstdbrv <kstdbrv1@gmail.com>
2020-11-01 12:47:41 +0300

    works on popup

--- MasterHands/frontend/src/components/service-body/ServiceBody.js
+++ MasterHands/frontend/src/components/service-body/ServiceBody.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './service-body.scss'
+import { useDispatch } from 'react-redux'
+import { showPopup } from '../../store/actions/app'
 
 
-const ServiceBody = ({price}) => {
+const ServiceBody = ({ price }) => {
+  
+  const dispatch = useDispatch();
+
   return (
   <div className="service-main">
     <div className="service">
@@ -31,7 +36,10 @@ const ServiceBody = ({price}) => {
       </p>&nbsp;
       <span>₽</span>
     </div>
-    <p className="service__question">
+    <p
+      className="service__question"
+      onClick={() => (dispatch(showPopup()))}   
+    >
       Как формируется цена?
     </p>
     <button className="button button-blue">
